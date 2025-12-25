ae0162c0bb4c659bf07eb6f398a54c1ce233f293
王晓磊 <m15232583691_1@163.com>
2025-12-25 14:33:52 +0800

    chore: art section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import gsap from "gsap";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
+import Art from "./components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +14,8 @@ const App = () => {
         <Navbar />
         <Hero />
         <Cocktails />
+        <About />
+        <Art />
     </main>
   );
 };
